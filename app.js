35f9128f77c55c83a5cc46a3ec18d840f5f05697
FabiolaCosta <fabiola.costa07@hotmail.com>
2025-02-03 21:37:39 -0300

    Add funcao atualizarListaAmigos

--- app.js
+++ app.js
@@ -11,4 +11,15 @@ function adicionarAmigo() {
     amigos.push(nome)
     document.getElementById('amigo').value = '';
     console.log(amigos)
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
